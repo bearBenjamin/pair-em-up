f80b474a61e519190afa8f67665044544721149f
Ilia Konev <122528494+bearBenjamin@users.noreply.github.com>
2026-02-04 21:05:12 +0300

    refactor: подправил пути до аудио файлов

--- js/audio.js
+++ js/audio.js
@@ -1,11 +1,11 @@
 import { state } from './state.js';
 
 const sounds = {
-  click: new Audio('../assets/sounds/click-btn.mp3'),
-  win: new Audio('../assets/sounds/win.mp3'),
-  lose: new Audio('../assets/sounds/lost.mp3'),
-  abort: new Audio('../assets/sounds/abort.mp3'),
-  happi: new Audio('../assets/sounds/happi.mp3'),
+  click: new Audio('./assets/sounds/click-btn.mp3'),
+  win: new Audio('./assets/sounds/win.mp3'),
+  lose: new Audio('./assets/sounds/lost.mp3'),
+  abort: new Audio('./assets/sounds/abort.mp3'),
+  happi: new Audio('./assets/sounds/happi.mp3'),
 };
 
 const playSound = (soundName) => {
